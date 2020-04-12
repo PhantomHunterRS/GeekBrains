00c50d8302e9ef6e1eda1e534850540e1250c837
phantomhunter <ASerFilyakov@mail.ru>
2020-04-12 13:47:10 +0300

    homeWork 3 completed

--- src/HomeWork3/HomeWork3.java
+++ src/HomeWork3/HomeWork3.java
@@ -15,6 +15,22 @@ public class HomeWork3 {
 
 //       Expression expression = new Expression("(2+3} - [2+0] - {4-5}");
 //        System.out.println(expression.checkBracket());
+//    MyQueue<Integer> queue =new MyQueue<Integer>(7);
+//        for (int i = 0; i < 7 ; i++) {
+//            queue.insert(i);
+//        }
+//        for (int i = 0; i <7 ; i++) {
+//            System.out.println(queue.remove());
+//        }
 
-    }
+        MyPriorityQueue<Integer> myPriorityQueue = new MyPriorityQueue<Integer>();
+        myPriorityQueue.insert(5);
+        System.out.println(myPriorityQueue);
+        myPriorityQueue.insert(15);
+        System.out.println(myPriorityQueue);
+        myPriorityQueue.insert(1);
+        System.out.println(myPriorityQueue);
+        myPriorityQueue.remove();
+        System.out.println(myPriorityQueue);
+   }
 }
